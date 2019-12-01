83bf26f4284006e468d1aa009d83e2ad48e3fe4c
Kevin Boyle <kevin@simb.co>
2019-11-30 17:39:19 -0800

    add new queries to raw sql query file

--- sql_queries/queries.sql
+++ sql_queries/queries.sql
@@ -1,7 +1,5 @@
 -- space for writing raw sql queries before knex wrapping
 
-
-
 SELECT * FROM restaurants AS rez left join likes_and_comments AS lc on rez.id=lc.restaurant_id;
 
 SELECT rez.id, rez.name, COUNT(lc.user_id) AS vote_count FROM restaurants AS rez left join likes_and_comments AS lc on rez.id=lc.restaurant_id GROUP BY rez.id;
@@ -9,3 +7,10 @@ SELECT rez.id, rez.name, COUNT(lc.user_id) AS vote_count FROM restaurants AS rez
 
 -- provides basic table with vote tally
 SELECT rez.*, COUNT(lc.user_id) AS vote_count FROM restaurants AS rez left join likes_and_comments AS lc on rez.id=lc.restaurant_id GROUP BY rez.id;
+
+
+-- provides full comment list, does not play nice with vote_count, but vote_count can be obtained by taking table.length
+SELECT lc.*, noms.* from likes_and_comments AS lc LEFT JOIN restaurants AS noms ON lc.restaurant_id=noms.id WHERE lc.restaurant_id=1;
+
+-- provides restaurant info w/ vote_count for single restaurant only
+SELECT noms.*, COUNT(lc.user_id) AS vote_count FROM restaurants AS noms LEFT JOIN likes_and_comments AS lc ON noms.id=lc.restaurant_id  WHERE noms.id=1 GROUP BY noms.id; 

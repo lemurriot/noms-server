fd20c1bdfb592b7034a3bd7f6fd3752404864c94
Kevin Boyle <kevin@simb.co>
2020-01-01 19:03:27 -0800

    bring app.js in compliance with eslint rules, and add commentsRouter

--- src/app.js
+++ src/app.js
@@ -1,37 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
-const restaurantsRouter = require('./restaurants/restaurants-router')
+const restaurantsRouter = require("./restaurants/restaurants-router");
+const commentsRouter = require("./comments/comments-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
-app.use('/api/restaurants', restaurantsRouter)
+app.use("/api/restaurants", restaurantsRouter);
+app.use("/api/comments", commentsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.get("/", (_req, res) => {
+  res.send("Hello, world!");
+});
 
-app.use(function errorHandler(error, req, res, next){
-  let response
-  if (NODE_ENV === 'production'){
-    response = { error: { message: 'server error' } }
+// eslint-disable-next-line prefer-arrow-callback
+app.use(function errorHandler(error, _req, res, _next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
   } else {
-    console.error(error)
-    response = { message: error.message, error }
+    // eslint-disable-next-line no-console
+    console.error(error);
+    response = { message: error.message, error };
   }
-  res.status(500).json(response)
-})
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;

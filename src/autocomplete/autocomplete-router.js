3566609b6605eb22990251421479628f0e8ce9e2
Kevin Boyle <kevin@simb.co>
2020-03-04 00:21:44 -0800

    clean up autocomplete router code

--- src/autocomplete/autocomplete-router.js
+++ src/autocomplete/autocomplete-router.js
@@ -2,17 +2,23 @@ const express = require("express");
 const autocompleteService = require("./autocomplete-service");
 
 const autocompleteRouter = express.Router();
-const jsonBodyParser = express.json();
 
 autocompleteRouter.route("/:querystring").get((req, res) => {
   autocompleteService
     .fetchResults(
       req.params.querystring
-      // TO DO reconfig for sessionToken from front end
+      // TO DO reconfig for sessionToken from frontend
+      // TO DO filter out non-restaurants in frontend
       // req.body.sessionToken
     )
     .then(response => response.json())
-    .then(response => res.send(response));
+    .then(response => res.send(response))
+    // TO DO flesh out error handling
+    .catch(
+      res.status(401).json({
+        message: "Could not fetch data"
+      })
+    );
 });
 
 module.exports = autocompleteRouter;

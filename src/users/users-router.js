1495801c9809a74f727d86d8f180df8b57dd3f29
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-03-06 16:45:18 -0800

    fix route for changing username

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -8,27 +8,27 @@ const jsonBodyParser = express.json();
 //   .get("/")
 
 usersRouter
-  .route("/change-name")
+  .route("/edit-username")
   .put(jsonBodyParser, async (req, res, next) => {
     const userNameExists = await UsersService.checkUserNameExists(
       req.app.get("db"),
-      req.body.username
+      req.body.new_username
     );
-    if (userNameExists) {
+    if (userNameExists.length) {
       res.status(404).json({
         error: "Username is already taken"
       });
+    } else {
+      UsersService.changeUsername(
+        req.app.get("db"),
+        req.body.email,
+        req.body.new_username
+      )
+        .then(user => {
+          res.status(204).json(user);
+        })
+        .catch(next);
     }
-
-    UsersService.changeUsername(
-      req.app.get("db"),
-      req.body.email,
-      req.body.username
-    )
-      .then(user => {
-        res.status(204).json(user);
-      })
-      .catch(next);
   });
 
 module.exports = usersRouter;

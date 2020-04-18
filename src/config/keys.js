a0e42426d1c03e4abd9dd5ca10d5ccda9feea277
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-04-18 16:17:34 -0700

    prepare for production w/ engines in pkg.json & keys hiddent to env variables

---
+++ src/config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    placesAPIKey: process.env.GOOGLE_PLACES_API_KEY
+  },
+  session: {
+    cookieKey: process.env.SESSION_COOKIE_KEY
+  }
+};

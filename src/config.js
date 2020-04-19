3108723171ecbe260adb0b4973de6d9edda4f9b7
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-04-18 18:47:49 -0700

    chage env variable names to match heroku specs

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB_URL: process.env.DB_URL || "postgresql://kevin@localhost/noms",
+  DATABASE_URL: process.env.DATABASE_URL || "postgresql://kevin@localhost/noms",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL || "postgresql://kevin@localhost/noms",
   NODE_ENV: process.env.NODE_ENV || "development",
   requestOrigin:
     process.env.NODE_ENV === "production"
--- src/knexfile.js
+++ src/knexfile.js
@@ -1,6 +1,6 @@
-const { DB_URL } = require("./config");
+const { DATABASE_URL } = require("./config");
 
 module.exports = {
   client: "pg",
-  connection: DB_URL
+  connection: DATABASE_URL
 };
